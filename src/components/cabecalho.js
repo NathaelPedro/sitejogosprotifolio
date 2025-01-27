70bcedd21b92ee1ddb917370f6729047c468b86d
Pedro Ayres <pedrocgb21@gmail.com>
2025-01-26 21:07:41 -0300

    adicionado a logo

--- src/components/cabecalho.js
+++ src/components/cabecalho.js
@@ -1,35 +1,39 @@
-const head=document.head
-const body=document.body
+const head = document.head;
+const body = document.body;
 
-const estilo="<link rel='stylesheet' type='text/css' href='./src/components/cabecalho.css'/>"
-head.innerHTML+=estilo
+const estilo = "<link rel='stylesheet' type='text/css' href='./src/components/cabecalho.css'/>";
+head.innerHTML += estilo;
 
-const topo=document.createElement("div")
-topo.setAttribute("id","topo")
-topo.setAttribute("class","topo")
-body.prepend(topo)
+const topo = document.createElement("div");
+topo.setAttribute("id", "topo");
+topo.setAttribute("class", "topo");
+body.prepend(topo);
 
-const logo=
-    "<div id='logo' class='logo'>"+
-        "<h1>Logo</h1>"+
-    "</div>"
-topo.innerHTML+=logo
+const logo = `
+    <div id='logo' class='logo'> 
+        <h1>
+            <img src='./logoSite.png' alt=' ' class='logo-img'>
+        </h1>
+    </div>
+`;
+topo.innerHTML += logo;
 
-const abas=
-    "<div id='abas' class='abas'>"+
-        "<ul id='aba' class='aba'>"+
-            "<li class='aba'><a  class='aba' href='index.html'>Home</a></li>"+
-            "<li class='aba'><a  class='aba' href='index.html'>Jogos</a></li>"+
-            "<li class='aba'><a  class='aba' href='index.html'>Reviews</a></li>"+
-            "<li class='aba'><a  class='aba' href='index.html'>Comunidades</a></li>"+
-        "</ul>"+
-    "</div>"
-topo.innerHTML+=abas
-    
-const lore=
-    "<div id='lore' class='lore'>"+
-        "<button id='login' class='loginRegister'>Login</button>"+
-        "<button id='register' class='loginRegister'>Register</button>"+
-    "</div>"
-topo.innerHTML+=lore
+const abas = `
+    <div id='abas' class='abas'>
+        <ul id='aba' class='aba'>
+            <li class='aba'><a class='aba' href='index.html'>Home</a></li>
+            <li class='aba'><a class='aba' href='index.html'>Jogos</a></li>
+            <li class='aba'><a class='aba' href='index.html'>Reviews</a></li>
+            <li class='aba'><a class='aba' href='index.html'>Comunidades</a></li>
+        </ul>
+    </div>
+`;
+topo.innerHTML += abas;
 
+const lore = `
+    <div id='lore' class='lore'>
+        <button id='login' class='loginRegister'>Login</button>
+        <button id='register' class='loginRegister'>Register</button>
+    </div>
+`;
+topo.innerHTML += lore;
